18a5537881a389a4119bd8ecb3f668484f5c1454
sadman-22 <sadmanarif5@gmail.com>
2025-03-14 16:40:07 +0600

    add videos

--- script/index.js
+++ script/index.js
@@ -2,14 +2,33 @@ console.log("index is connected");
 
 const loadCategories = () => {
     // console.log("category is loading") 
-    // fetch 
+    // 1 fetch tha data 
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    // convert to json
     .then(res => res.json())
-    .then(data => displayCategories(data.categories))
+    // send data to display
+    .then(data => displayCategories(data.categories));
 }
 
+// {
+//     "category_id": "1001",
+//     "category": "Music"
+// }
+
 const displayCategories = (categories) => {
-    console.log(categories);
+    // 1.get the container
+    const categoryContainer = document.getElementById("category-container");
+    //2.loop operation on Array of object
+    for (let cat of categories) {
+        // console.log(cat);
+        // 3.create element
+        const categoryDiv = document.createElement("div");
+        categoryDiv.innerHTML = `
+        <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        `;
+        // 4.appended the element
+        categoryContainer.append(categoryDiv);
+    }
 }
 
 loadCategories()
